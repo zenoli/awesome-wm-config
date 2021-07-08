8a9e362ebd961aab4edd40e975fee3f751963adc
olivier <olibitter@gmail.com>
2021-07-08 16:15:47 +0200

    Add submodules

--- theme.lua
+++ theme.lua
@@ -6,7 +6,7 @@
 --]]
 
 local gears = require("gears")
-local lain  = require("lain")
+local lain  = require("submodules.lain")
 local awful = require("awful")
 local wibox = require("wibox")
 local dpi   = require("beautiful.xresources").apply_dpi
