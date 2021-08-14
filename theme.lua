5eb804529d80137ffa229d1800ff640f9ab2646f
olivier <olibitter@gmail.com>
2021-08-14 18:13:28 +0200

    Cleanup theme.lua

--- theme.lua
+++ theme.lua
@@ -3,32 +3,48 @@ local dpi   = require("beautiful.xresources").apply_dpi
 
 
 local theme                                     = {}
+
+---------------------------------------
+-- General
+---------------------------------------
+-- Fonts
 theme.font_size                                 = 9
+theme.font                                      = "Terminus "       .. theme.font_size
+theme.taglist_font                              = "Hack Nerd Font " .. theme.font_size
+
+-- Misc
+theme.useless_gap                               = 6
+theme.notification_icon_size                    = 30
+theme.notification_border_width                 = 10
+theme.border_width                              = dpi(0)
+theme.systray_icon_spacing                      = dpi(3)
+theme.menu_height                               = dpi(19)
+theme.menu_width                                = dpi(170)
+theme.tasklist_plain_task_name                  = false
+theme.tasklist_disable_icon                     = true
+
+
+-- Paths
 theme.dir                                       = os.getenv("HOME") .. "/.config/awesome"
 theme.icon_dir                                  = os.getenv("HOME") .. "/.config/awesome/icons"
 theme.wallpaper                                 = theme.dir .. "/default_wallpaper.jpg"
-theme.font                                      = "Terminus "       .. theme.font_size
-theme.taglist_font                              = "Hack Nerd Font " .. theme.font_size
+
+---------------------------------------
+-- Colors
+---------------------------------------
 theme.fg_normal                                 = "#FEFEFE"
 theme.fg_focus                                  = "#32D6FF"
 theme.fg_urgent                                 = "#C83F11"
 theme.bg_normal                                 = "#22222290"
 theme.bg_systray                                = "#00000000"
-theme.systray_icon_spacing = dpi(3)
--- theme.bg_normal                                 = "#00000000"
 theme.bg_focus                                  = "#1E232000"
--- theme.bg_focus                                  = "#00000000"
 theme.bg_urgent                                 = "#3F3F3F"
 theme.taglist_fg_focus                          = "#F6784F90"
 theme.tasklist_bg_focus                         = "#22222200"
-theme.tasklist_bg_normal                         = "#00000000"
+theme.tasklist_bg_normal                        = "#00000000"
 theme.tasklist_fg_focus                         = "#F6784F"
-theme.border_width                              = dpi(0)
-theme.notification_icon_size = 30
-theme.notification_border_width = 10
-theme.notification_border_color = "#000000"
+theme.notification_border_color                 = "#000000"
 theme.border_normal                             = "#3F3F3F00"
--- theme.border_focus                              = "#6F6F6F"
 theme.border_focus                              = theme.bg_focus
 theme.border_marked                             = "#CC9393"
 theme.titlebar_bg_focus                         = "#3F3F3F"
@@ -36,13 +52,20 @@ theme.titlebar_bg_normal                        = "#3F3F3F"
 theme.titlebar_bg_focus                         = theme.bg_focus
 theme.titlebar_bg_normal                        = theme.bg_normal
 theme.titlebar_fg_focus                         = theme.fg_focus
-theme.menu_bg_normal = "#22222240"
-theme.menu_height                               = dpi(19)
-theme.menu_width                                = dpi(170)
+theme.menu_bg_normal                            = "#22222240"
+
+---------------------------------------
+-- Icons
+---------------------------------------
+--General
 theme.menu_submenu_icon                         = theme.icon_dir .. "/submenu.png"
 theme.awesome_icon                              = theme.icon_dir .. "/awesome.png"
+
+-- Taglist
 theme.taglist_squares_sel                       = theme.icon_dir .. "/square_sel.png"
 theme.taglist_squares_unsel                     = theme.icon_dir .. "/square_unsel.png"
+
+-- Layout
 theme.layout_tile                               = theme.icon_dir .. "/layouts/tile.png"
 theme.layout_tileleft                           = theme.icon_dir .. "/layouts/tileleft.png"
 theme.layout_tilebottom                         = theme.icon_dir .. "/layouts/tilebottom.png"
@@ -55,6 +78,8 @@ theme.layout_max                                = theme.icon_dir .. "/layouts/ma
 theme.layout_fullscreen                         = theme.icon_dir .. "/layouts/fullscreen.png"
 theme.layout_magnifier                          = theme.icon_dir .. "/layouts/magnifier.png"
 theme.layout_floating                           = theme.icon_dir .. "/layouts/floating.png"
+
+-- Widgets
 theme.widget_ac                                 = theme.icon_dir .. "/widgets/ac.png"
 theme.widget_battery                            = theme.icon_dir .. "/widgets/battery.png"
 theme.widget_battery_low                        = theme.icon_dir .. "/widgets/battery_low.png"
@@ -77,9 +102,8 @@ theme.widget_mail                               = theme.icon_dir .. "/widgets/ma
 theme.widget_mail_on                            = theme.icon_dir .. "/widgets/mail_on.png"
 theme.widget_task                               = theme.icon_dir .. "/widgets/task.png"
 theme.widget_scissors                           = theme.icon_dir .. "/widgets/scissors.png"
-theme.tasklist_plain_task_name                  = false
-theme.tasklist_disable_icon                     = true
-theme.useless_gap                               = 6
+
+-- Titlebar
 theme.titlebar_close_button_focus               = theme.icon_dir .. "/titlebar/close_focus.png"
 theme.titlebar_close_button_normal              = theme.icon_dir .. "/titlebar/close_normal.png"
 theme.titlebar_ontop_button_focus_active        = theme.icon_dir .. "/titlebar/ontop_focus_active.png"
@@ -98,7 +122,5 @@ theme.titlebar_maximized_button_focus_active    = theme.icon_dir .. "/titlebar/m
 theme.titlebar_maximized_button_normal_active   = theme.icon_dir .. "/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_inactive  = theme.icon_dir .. "/titlebar/maximized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_inactive = theme.icon_dir .. "/titlebar/maximized_normal_inactive.png"
-theme.n_workspace_tags = 5
-theme.workspace_tag_default_layout = awful.layout.suit.tile
 
 return theme
