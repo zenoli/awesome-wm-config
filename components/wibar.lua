9cc8b043df694deba77b6f9b1a023ef4dbb352bf
olivier <olibitter@gmail.com>
2022-09-24 17:53:45 +0200

    Implement rounded border taglist

--- components/wibar.lua
+++ components/wibar.lua
@@ -6,6 +6,8 @@ local taglist_buttons = require("bindings.taglist_buttons")
 local tasklist_buttons = require("bindings.tasklist_buttons")
 local widgetlist = require("components.widgetlist")
 local beautiful = require("beautiful")
+local gears            = require("gears")
+local colors    = require("constants.colors")
 
 
 local function setup(s)
@@ -13,7 +15,11 @@ local function setup(s)
 
     -- Create a promptbox for each screen
     s.promptbox = awful.widget.prompt()
-    s.taglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist_buttons)
+    s.taglist = awful.widget.taglist {
+        screen = s,
+        filter = awful.widget.taglist.filter.all,
+        buttons = taglist_buttons
+    }
 
     -- Create a tasklist widget
     -- s.tasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons)
@@ -33,7 +39,22 @@ local function setup(s)
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
-            s.taglist,
+            {
+                {
+                    {
+                        s.taglist,
+                        left = 5,
+                        right = 5,
+                        widget = wibox.container.margin
+                    },
+                    -- s.taglist,
+                    shape = gears.shape.rounded_bar,
+                    bg = colors.black .. "30",
+                    widget = wibox.container.background
+                },
+                margins = 5,
+                widget = wibox.container.margin
+            },
             s.promptbox,
         },
         s.tasklist, -- Middle widget
